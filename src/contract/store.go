120da9b9103c7061699cb7a4c93a38947c01839c
NoobforAl <58856931+NoobforAl@users.noreply.github.com>
2024-06-30 20:03:35 +0330

    add: break contract store and add some function for somme service!
    Signed-off-by: NoobforAl <58856931+NoobforAl@users.noreply.github.com>

--- src/contract/store.go
+++ src/contract/store.go
@@ -6,13 +6,32 @@ import (
 	"github.com/NoobforAl/real_time_chat_application/src/entity"
 )
 
+type StoreUser interface {
+	User(ctx context.Context, username string) (entity.User, error)
+	CreateUser(ctx context.Context, userData entity.User) (entity.User, error)
+	UpdateUser(ctx context.Context, id string, updatedData entity.User) (entity.User, error)
+}
+
+type StoreRoom interface {
+	Messages(ctx context.Context, roomId string, maxLen int) ([]*entity.Message, error)
+	CreateMessage(ctx context.Context, message entity.Message) (entity.Message, error)
+}
+type StoreMessage interface {
+	Rooms(ctx context.Context, maxLen int) ([]*entity.Room, error)
+	CreateRoom(ctx context.Context, message entity.Room) (entity.Room, error)
+}
+
+type StoreNotifications interface {
+	Notifications(ctx context.Context, maxLen int) ([]*entity.Notification, error)
+	CreateNotification(ctx context.Context, message entity.Notification) (entity.Notification, error)
+
+	CreateNotificationRoom(ctx context.Context, notificationRoomData entity.NotificationRoom) (entity.NotificationRoom, error)
+	UpdateNotificationRoom(ctx context.Context, message entity.NotificationRoom) (entity.NotificationRoom, error)
+}
+
 type Store interface {
-	User(ctx context.Context, id string) entity.User
-	CreateUser(ctx context.Context, userData entity.User) entity.User
-
-	Messages(ctx context.Context, roomId string, maxLen int) []*entity.Message
-	CreateMessage(ctx context.Context, message entity.Message) entity.Message
-
-	Rooms(ctx context.Context, maxLen int) []*entity.Room
-	CreateRoom(ctx context.Context, message entity.Room) entity.Room
+	StoreUser
+	StoreRoom
+	StoreMessage
+	StoreNotifications
 }
